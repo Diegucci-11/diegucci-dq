1de69a362b6e078f050bbaad41c94ff61d0ffd93
Diegucci <diegucci.sautter@gmail.com>
2024-01-21 19:39:30 +0100

    Added new module

---
+++ IaC/modules/serviceaccount/main.tf
@@ -0,0 +1,39 @@
+resource "google_service_account" "data_quality_service_account" {
+  account_id   = var.service_account
+  display_name = "DQ service account"
+}
+
+resource "google_project_iam_member" "editor_role" {
+  project = var.id_project
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.data_quality_service_account.email}"
+}
+
+resource "google_project_iam_member" "token_creator_role" {
+  project = var.project
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:${google_service_account.data_quality_service_account.email}"
+}
+
+resource "google_project_iam_member" "secret_accessor_role" {
+  project = var.project
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.data_quality_service_account.email}"
+}
+
+resource "google_service_account_key" "my_service_account_key" {
+  service_account_id = google_service_account.data_quality_service_account.name
+}
+
+resource "google_secret_manager_secret" "my_service_account_secret" {
+  secret_id = var.name_secret
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "my_service_account_secret_version" {
+  secret = google_secret_manager_secret.my_service_account_secret.id
+  secret_data = google_service_account_key.my_service_account_key.private_key
+} 
---
+++ IaC/modules/serviceaccount/variables.tf
@@ -0,0 +1,15 @@
+variable "id_project" {
+  description = "Id project GCP"
+  type        = string
+}
+
+variable "service_account" {
+  description = "Service account"
+  type        = string
+}
+
+variable "name_secret" {
+  description = "Name for the secret of the service-account key"
+  type        = string
+}
+
