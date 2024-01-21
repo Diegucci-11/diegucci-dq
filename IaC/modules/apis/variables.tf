8c59c809b2ddbf9dada780f5c9e7b56ebbd53e18
Diegucci <diegucci.sautter@gmail.com>
2024-01-21 18:19:19 +0100

    test 2 fixed bug

--- IaC/modules/apis/variables.tf
+++ IaC/modules/apis/variables.tf
@@ -2,3 +2,8 @@ variable "apis_list"{
   description = "Required APIs for the Data Quality Platform"
   type        = list(string)
 }
+
+variable "id_project" {
+  description = "Id project GCP"
+  type        = string
+}
