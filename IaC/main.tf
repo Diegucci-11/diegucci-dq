995a88ab818df97141db1720343de906ccb2041e
Diegucci <diegucci.sautter@gmail.com>
2024-01-26 16:55:35 +0100

    Trying resolving variables file

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,7 +1,32 @@
-# provider "google" {
-#   project     = var.id_project
-#   region      = var.region
-# }
+provider "google" {
+  project     = var.id_project
+  region      = var.region
+}
+
+locals {
+  service_accounts = {
+    terraform_admin = {
+      id             = "terraform-admin",
+      email          = "terraform-admin@${var.project_id}.iam.gserviceaccount.com"
+      roles_to_grant = []
+    }
+  }
+
+  services_to_enable = [
+    "cloudfunctions.googleapis.com",
+    "run.googl.googleapis.com",
+    "iam.googleeapis.com",
+    "logging.googleapis.com",
+    "sheets.googleapis.com",
+    "drive.googleapis.com",
+    "cloudscheduler.googleapis.com",
+    "secretmanager.googleapis.com",
+    "connectors.googleapis.com",
+    "pubsub.googleapis.com",
+    "eventarc.googleapis.com",
+    "cloudbuildapis.com",
+  ]
+}
 
 # module "apis"{
 #   source = "./modules/apis"
