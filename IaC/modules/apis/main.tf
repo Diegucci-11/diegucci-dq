479728d88c9fe5bf49c66ed6ac2c64d049e74a98
Diegucci <diegucci.sautter@gmail.com>
2024-01-26 17:29:49 +0100

    Trying resolving variables file

--- IaC/modules/apis/main.tf
+++ IaC/modules/apis/main.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project     = var.id_project
+  region      = var.region
+}
+
 resource "google_project_service" "enable_apis" {
   project = var.id_project
   for_each = toset(var.apis_list)
