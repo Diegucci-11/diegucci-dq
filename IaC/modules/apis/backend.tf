42875b5d48b57c303b2ceb6985a7da509652af10
Diegucci <diegucci.sautter@gmail.com>
2024-01-26 17:26:43 +0100

    Trying resolving variables file

--- IaC/modules/apis/backend.tf
+++ IaC/modules/apis/backend.tf
@@ -4,15 +4,15 @@ terraform {
     prefix  = "tfstate-pubsub"
   }
 
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "4.78.0"
-    }
-  }
-  required_version = ">= 0.13"
+  # required_providers {
+  #   google = {
+  #     source  = "hashicorp/google"
+  #     version = "~> 4.0"
+  #   }
+  #   google-beta = {
+  #     source  = "hashicorp/google-beta"
+  #     version = "4.78.0"
+  #   }
+  # }
+  # required_version = ">= 0.13"
 }
