df7efc9098e0383b87e6486765b5afc903f37bf2
Diegucci <diegucci.sautter@gmail.com>
2024-01-27 18:56:41 +0100

    Testing qae_publisher

--- Codigo/YML_Publisher/yml_publisher/main.py
+++ Codigo/YML_Publisher/yml_publisher/main.py
@@ -18,7 +18,8 @@ import functions_framework
 def yml_publisher(request):
     SCOPES = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     dateNow_name = datetime.datetime.now().strftime('%Y_%m_%d_%H%M%S')
-    file_name = "YML_"+dateNow_name+".yml"
+    # file_name = "YML_"+dateNow_name+".yml"
+    file_name = "yml_test.yml"
 
     credentials = service_account.Credentials.from_service_account_info(json.loads(os.environ.get('DQ_KEY')), scopes=SCOPES)
     client = gspread.authorize(credentials)
@@ -27,29 +28,29 @@ def yml_publisher(request):
     
     reglas = spreadsheet.worksheet('Reglas')
     rules = reglas.range('K2:K')
-    i=0
-    for rule in rules:
-        if(i > 4): 
-            return
-        print(rule)
-        i+=1
     
     filtros = spreadsheet.worksheet('Filtros_Aut')
     filters = filtros.range('D3:D')
 
     output_yaml = "rule_dimensions:\n\t- Exactitud\n\t- Completitud\n\t- Consistencia\n\t- Integridad\n\t- Disponibilidad\n\t- Unicidad\n\t- Validez\n\n"
+    print(output_yaml)
+    
     output_yaml += "row_filters: \n"
     for filter in filters:
         output_yaml += "\t" + filter + "\n\n"
+    
+    print(output_yaml)
 
     output_yaml += "rules: \n"
     for rule in rules:
         output_yaml += "\t" + rule + "\n\n"
     
+    print(output_yaml)
+
     output_yaml += "rule_bindings: \n"
     
     upload_blob(os.environ.get('YML_BUCKET'), output_yaml, file_name)
-
+    
     # Incluir el número de la pestaña de la plantilla, donde está situada "yaml_semifinal", empezando por 0
     # sheet_instance_tablas = sheet.get_worksheet(15)
     # values_list = sheet_instance_tablas.col_values(1)
@@ -59,7 +60,7 @@ def yml_publisher(request):
     # export_yaml.Upload()
     # with open(file_name, 'w') as f:
     #     f.write(output_list)
-
+    
     return ""
 
 def upload_blob(bucket_name, output_list, destination_blob_name):
