92a0e0d1fd12878d049e61f68699f0907f7ed6c1
Diegucci <diegucci.sautter@gmail.com>
2024-01-27 15:00:46 +0100

    Testing qae_publisher

--- Codigo/QAE/qae_publisher/main.py
+++ Codigo/QAE/qae_publisher/main.py
@@ -16,13 +16,16 @@ import os
 
 @functions_framework.http
 def qae_publisher(request):
+    print("Entro en la función")
     SCOPES = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     # id_drive_repo = '1hWEdMgihOB4UU6Z7q0LltyC5tQ_cSFqM'
+    print("Defino scopes")
     credentials = service_account.Credentials.from_service_account_info(json.loads(os.environ.get('DQ_KEY')), scopes=SCOPES)
-
+    print("cargo la variable credentials")
     # credentials_json = json.loads(get_password('data_quality_key'))
     # credentials = service_account.Credentials.from_service_account_info(credentials_json, scopes=SCOPES)
     client = gspread.authorize(credentials)
+    print("Autorizo credentials")
     # gauth = GoogleAuth()
     # gauth.credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_json, SCOPES)
     # drive = GoogleDrive(gauth)
