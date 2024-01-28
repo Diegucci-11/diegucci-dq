36ecf3242fc3db8a9cd225da88e2442763c53fab
Diegucci <diegucci.sautter@gmail.com>
2024-01-28 14:33:30 +0100

    Deploying and optimizing qid_pub

--- IaC/modules/composer/outputs.tf
+++ IaC/modules/composer/outputs.tf
@@ -1,3 +1,3 @@
 output "composer_bucket_name" {
-  value = google_composer_environment.environment_creation.config.gcs_bucket
+  value = google_composer_environment.environment_creation.config[0].gcs_bucket
 }
