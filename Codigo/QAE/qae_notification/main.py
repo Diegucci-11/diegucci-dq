5ec07a6d5186050061ef539f932beccf7a437366
Diegucci <diegucci.sautter@gmail.com>
2024-02-03 17:18:13 +0100

    Trying QAE notification

--- Codigo/QAE/qae_notification/main.py
+++ Codigo/QAE/qae_notification/main.py
@@ -42,9 +42,7 @@ def qae_notification(request):
     print(df_correos)
     for indice_fila, fila in enumerate(correos.get_all_values()):
         if fila[1] != '' and fila[1] is not None:
-            print("printeo fila3...")
-            print(fila[3][0:1])
-            if fila[1] not in correos_utilizados and int(fila[3][0:1]) in severidad and fila[2] == env:
+            if fila[1] not in correos_utilizados and (int(fila[3][0:1]) in severidad) and fila[2] == env:
                 print("Entro en segundo if")
                 enviarCorreo(fila[0], fila[1], fila[2], fila[3], product)
                 correos_utilizados[i] = fila[1]
