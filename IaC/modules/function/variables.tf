4261d78177334477fe8c27802a6db98dd00893e1
Diegucci <diegucci.sautter@gmail.com>
2024-02-06 21:18:59 +0100

    Trying QAE notification

--- IaC/modules/function/variables.tf
+++ IaC/modules/function/variables.tf
@@ -19,11 +19,6 @@ variable "region" {
   type        = string
 }
 
-variable "service_account" {
-  description = "Service account"
-  type        = string
-}
-
 variable "name_secret" {
   description = "Name for the secret of the service-account key"
   type        = string
