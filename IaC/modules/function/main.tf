ca538a82fb398cd7ec17b685ea5c5106a57606c4
Diegucci <diegucci.sautter@gmail.com>
2024-02-06 22:23:44 +0100

    Trying QAE notification

--- IaC/modules/function/main.tf
+++ IaC/modules/function/main.tf
@@ -27,7 +27,7 @@ resource "google_cloudfunctions2_function" "qid_notification" {
   location      = var.region
   build_config {
     runtime     = var.programming_language
-    entry_point = "main"
+    entry_point = "qid_publisher"
     source {
       storage_source {
         bucket  = var.name_qid_bucket
@@ -41,7 +41,7 @@ resource "google_cloudfunctions2_function" "qid_notification" {
     available_cpu      = "0.583"
     timeout_seconds    = 500
     environment_variables = {
-      QID_BUCKET    = var.name_qid_buckets
+      QID_BUCKET    = var.name_qid_bucket
       QID_SQL       = "qid_sql.sql"
       MATRIX_FILE   = "MatrixInput_v1.1"
     }
