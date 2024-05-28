8f1acb377ed1b93e78277a2d13254a2b1672a3d7
Diegucci <diegucci.sautter@gmail.com>
2024-05-28 14:01:32 +0200

    deploying in new project

--- IaC/modules/composer/main.tf
+++ IaC/modules/composer/main.tf
@@ -8,33 +8,33 @@ resource "google_composer_environment" "environment_creation" {
   region = var.region_composer
   config {
     software_config {
-      image_version = "composer-2-airflow-2"
+      image_version = "composer-2.8.0-airflow-2.7.3"
 
       pypi_packages = {
         gspread = ""
       }
     }
 
-    workloads_config {
-      scheduler {
-        cpu        = 0.5
-        memory_gb  = 1.875
-        storage_gb = 1
-        count      = 1
-      }
-      web_server {
-        cpu        = 0.5
-        memory_gb  = 1.875
-        storage_gb = 1
-      }
-      worker {
-        cpu = 0.5
-        memory_gb  = 1.875
-        storage_gb = 1
-        min_count  = 1
-        max_count  = 3
-      }
-    }
+    # workloads_config {
+    #   scheduler {
+    #     cpu        = 0.5
+    #     memory_gb  = 1.875
+    #     storage_gb = 1
+    #     count      = 1
+    #   }
+    #   web_server {
+    #     cpu        = 0.5
+    #     memory_gb  = 1.875
+    #     storage_gb = 1
+    #   }
+    #   worker {
+    #     cpu = 0.5
+    #     memory_gb  = 1.875
+    #     storage_gb = 1
+    #     min_count  = 1
+    #     max_count  = 3
+    #   }
+    # }
     environment_size = "ENVIRONMENT_SIZE_SMALL"
 
     node_config {
